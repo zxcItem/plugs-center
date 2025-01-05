3724c4d98a6095149052607a6348f5ba0566991f
zxcItem <1262847141@qq.com>
2025-01-05 09:52:15 +0800

    Update 20230921000001_install_center_data.php

--- stc/database/20230921000001_install_center_data.php
+++ stc/database/20230921000001_install_center_data.php
@@ -3,17 +3,28 @@
 use think\admin\extend\PhinxExtend;
 use think\migration\Migrator;
 
+@set_time_limit(0);
+@ini_set('memory_limit', -1);
+
 class InstallCenterData extends Migrator
 {
+    /**
+     * 创建数据库
+     * @throws \Exception
+     */
     public function change()
     {
-        set_time_limit(0);
-        @ini_set('memory_limit', -1);
         $this->insertMenu();
     }
 
+    /**
+     * 初始化系统菜单
+     * @return void
+     * @throws \Exception
+     */
     private function insertMenu()
     {
+        // 初始化菜单数据
         PhinxExtend::write2menu([
             [
                 'name' => '插件入口',
